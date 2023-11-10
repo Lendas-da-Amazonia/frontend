b09fbb05946d92ec8ae1aa92e5db333569100c3c
miqueiasvianasilva21 <miqueias@icomp.ufam.edu.br>
2023-11-10 08:18:03 -0400

    corrigindo o adicionar imagem

--- src/pages/Legend/Create.tsx
+++ src/pages/Legend/Create.tsx
@@ -87,7 +87,7 @@ const Create = () => {
   }
 
   return (
-    <div className="h-full bg-slate-900 flex justify-center">
+    <div className="h-full bg-slate-950 flex justify-center">
       <div className="w-full max-w-3xl py-10 text-white flex flex-col gap-7">
         <div className="col-span-3 h-fit">
           <Label>Título</Label>
@@ -104,43 +104,31 @@ const Create = () => {
         </div>
 
         <div className="col-span-3 h-fit">
-          <Label>Capa da lenda</Label>
-          <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-            <div className="space-y-1 text-center">
-              <svg
-                className="mx-auto h-12 w-12 text-gray-400"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-              >
-                <path
-                  d="M10 14v20a4 4 0 004 4h20a4 4 0 004-4V14m2-7a2 2 0 012 2h16a2 2 0 012-2M24 32a2 2 0 01-2-2a2 2 0 012-2a2 2 0 012 2a2 2 0 01-2 2m-4-7h8"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <div className="flex text-sm text-gray-600">
-                <label
-                  htmlFor="image"
-                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                >
-                  <span>Selecione uma imagem</span>
-                  <input
-                    id="image"
-                    accept="image/*"
-                    type="file"
-                    className="sr-only"
-                    onChange={convertToBase64}
-                  />
-                </label>
-                {image && <img src={image} alt="Imagem selecionada" className="mt-2 w-[200px] h-[150px]" />}
-              </div>
-              <p className="text-xs text-gray-500">ou arraste e solte uma imagem</p>
-            </div>
-          </div>
-        </div>
+  <Label>Capa da lenda</Label>
+  <div className="mt-1 p-2 border border-white rounded-md flex flex-col items-center justify-center">
+    {image && (
+      <img src={image} alt="Imagem selecionada" className="w-[200px] h-[150px]" />
+    )}
+    <div className="flex text-sm text-gray-600">
+      <label
+        htmlFor="image"
+        className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+      >
+        <span>Selecione uma imagem</span>
+        <input
+          id="image"
+          accept="image/*"
+          type="file"
+          className="sr-only"
+          onChange={convertToBase64}
+        />
+      </label>
+    </div>
+    
+  </div>
+</div>
+
+
 
         <div className="col-span-1 h-fit mt-10">
           <Button onClick={handlePusblish}>Publicar</Button>
